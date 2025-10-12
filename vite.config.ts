6df94fe3b03dcfc8ea362587328f9a4bc454de50
RoB8080 <75566977@qq.com>
2025-10-13 03:10:16 +0800

    build: integrate vercel functions

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import path from "node:path";
 import tailwindcss from "@tailwindcss/vite";
 import { tanstackRouter } from "@tanstack/router-plugin/vite";
 import svgr from "vite-plugin-svgr";
+import vercel from "vite-plugin-vercel";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -20,10 +21,20 @@ export default defineConfig({
     }),
     tailwindcss(),
     svgr(),
+    vercel(),
   ],
   resolve: {
     alias: {
       "@": path.join(path.resolve(), "src"),
+    },
+  },
+  server: {
+    port: process.env.PORT as unknown as number,
+    proxy: {
+      "/api": {
+        target: "http://localhost:3000",
+        changeOrigin: true,
+      },
     },
   },
 });
